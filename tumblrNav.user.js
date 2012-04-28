7eab5c82083210980edbd2cd3acc40a9e70ba30c
Pax Unix <paxunix@gmail.com>
2012-04-28 10:13:27 -0700

    Don't navigate if focussed element is in a form
    - otherwise your cursor keys won't work because they cause page
      navigation

--- tumblrNav.user.js
+++ tumblrNav.user.js
@@ -3,10 +3,16 @@
 // @namespace       halpenny
 // @description     Map left/right arrow keys to prev page/next page navigation.
 // @include         http://*.tumblr.com/*
-// @version         0.1
+// @version         1.0.1
 // ==/UserScript==
 
 document.body.onkeydown = function(evt) {
+
+// No action if the active element belongs to a form.  This is an attempt to
+// prevent cursor movements within form elements from triggering the
+// location change.
+if (document.activeElement.form)
+    return;
 
 var els = document.location.pathname.split(/\//);
 
@@ -15,6 +21,16 @@ if (els.length == 2 || els[1] && els[1] === "page")
     var curPage = +(els[2] || 1);
     var nextPage = curPage + 1;
     var prevPage = Math.max(1, curPage - 1);
+
+    // No action if any key modifiers are on
+    if (evt.ctrlKey ||
+        evt.shiftKey ||
+        evt.altKey ||
+        evt.metaKey ||
+        evt.altGraphKey)
+    {
+        return;
+    }
 
     if (evt.which == 39)   // right arrow
     {
